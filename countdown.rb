f1fb066910ceae5de349897be4778b1fd093933f
Adam Barton <adam.barton333@gmail.com>
2018-05-12 19:01:18 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
 #write your code here
 
-def countdown
-  counter = 20
+def countdown(10)
+  counter = 
   
   until counter = 0 
   puts "#{counter} SECOND(S)!"
+  counter -= 1 
 end
 return "HAPPY NEW YEAR!"
 end
