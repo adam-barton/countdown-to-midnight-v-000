a2410f821b2a772477725f2bdf72b30e4ee8fded
Adam Barton <adam.barton333@gmail.com>
2018-05-12 19:12:14 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,6 +10,13 @@ return "HAPPY NEW YEAR!"
 end
 
 
-def countdown_with_sleep
+def countdown_with_sleep(x)
+  x = 10 
+  
+  until x == 0 
+   puts "#{x} SECOND(S)!"
+   x -= 1 
+ end
+ return "HAPPY NEW YEAR!"
   
 end
